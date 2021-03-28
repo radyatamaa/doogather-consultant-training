a8a306836266558e73ce819f337ae02719b46ee7
radyatamaa <43398711+radyatamaa@users.noreply.github.com>
2021-03-28 23:20:02 +0700

    initital

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+// Created By @hoaaah * Copyright (c) 2020 belajararief.com
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=doogather-consultant-training',
+    'username' => 'root',
+    'password' => 'root',
+    'charset' => 'utf8',
+];
